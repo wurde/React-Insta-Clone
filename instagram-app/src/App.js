b5d5ac862eb1fee8c4054f8315e380c2dca2f7f1
Andy Bettisworth <andy@accreu.com>
2019-04-29 12:32:37 -0500

    Remove unused files and format app

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+
+/**
+ * Import component styles
+ */
+
+require('./App.css')
+
+/**
+ * Define component
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Instagram</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+/**
+ * Export component
+ */
+
+module.exports = App
