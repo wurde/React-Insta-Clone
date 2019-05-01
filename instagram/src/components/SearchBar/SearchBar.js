5eabc6572b6ecc27c8326f40c61b59919a952fb3
Andy Bettisworth <andy@accreu.com>
2019-05-01 17:30:49 -0500

    Add logout functionality

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -15,6 +15,11 @@ require('./SearchBar.scss')
  */
 
 function SearchBar(props) {
+  function logoutUser() {
+    window.localStorage.removeItem('username')
+    window.location.reload()
+  }
+
   return (
     <header className="jsx-SearchBar">
       <div className="container">
@@ -33,7 +38,7 @@ function SearchBar(props) {
           <div className="col-3 d-flex align-items-center">
             <i className="far fa-compass fa-2x jsx-SearchBar__explore"></i>
             <i className="far fa-heart fa-2x jsx-SearchBar__activity"></i>
-            <i className="far fa-user fa-2x jsx-SearchBar__profile"></i>
+            <i className="far fa-user fa-2x jsx-SearchBar__profile" onClick={logoutUser}></i>
           </div>
         </div>
       </div>
